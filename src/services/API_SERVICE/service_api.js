2f406d6dc483c2549d8393a69db5ba100fa0737e
Konstantin-O-B <105279265+Konstantin-O-B@users.noreply.github.com>
2023-09-28 01:13:40 +0700

    Delete src/services directory

--- src/services/API_SERVICE/service_api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export default async function getResources(url) {
-  const { data } = await axios.get(url);
-  /* console.log(data); */
-  return data;
-}
--- src/services/LS_SERVICE/service_ls.js
+++
@@ -1,7 +0,0 @@
-export function setItemToLS(key, item) {
-  localStorage.setItem(key, item);
-}
-
-export function getItemFromLS(key) {
-  return localStorage.getItem(key);
-}
